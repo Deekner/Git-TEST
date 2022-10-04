3c2f9c0c4712a45f3b1d0600605ad76f86ed5ab5
Dennis Dan-I <Dennis Dan-I@LAPTOP-TAORQUDE>
2022-10-04 09:28:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Git_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello Git");
+
+        }
+    }
+}
